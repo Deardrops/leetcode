00f9ffa3716eb134f42774fc4fb49aabc4614968
deardrops <passagesfall@gmail.com>
2019-04-02 09:32:52 +0800

    2019/04/02

--- top-interview-questions-easy/array/best-time-to-buy-and-sell-stock-ii/solution.go
+++ top-interview-questions-easy/array/best-time-to-buy-and-sell-stock-ii/solution.go
@@ -1,5 +1,23 @@
 package main
 
-func maxProfit(prices []int) int {
+import "fmt"
 
+/*
+	解题思路：题意 = 每天只要比前一天的价格低，就卖出
+*/
+func maxProfit(prices []int) int {
+	sum := 0
+	for i := 1; i < len(prices); i++ {
+		diff := prices[i] - prices[i-1]
+		if diff > 0 {
+			sum += diff
+		}
+	}
+	return sum
+}
+
+func main() {
+	prices := []int{7, 1, 5, 6, 3, 6}
+	res := maxProfit(prices)
+	fmt.Println(res)
 }
