121dbbe55441a936d747bbfd637ee86052966f5e
deardrops <passagesfall@gmail.com>
2020-03-22 21:40:24 +0800

    2020/03/22

--- top-interview-questions-medium/others/evaluate-reverse-polish-notation/solution.go
+++ top-interview-questions-medium/others/evaluate-reverse-polish-notation/solution.go
@@ -1,5 +1,72 @@
 package main
 
-func evalRPN(tokens []string) int {
+import (
+	"fmt"
+	"strconv"
+)
 
+func evalRPN(tokens []string) int {
+	s := newStack()
+	for _, token := range tokens {
+		if isOperator(token) {
+			right := s.pop()
+			left := s.pop()
+			res := doOperator(left, right, token)
+			s.push(res)
+		} else {
+			num, _ := strconv.Atoi(token)
+			s.push(num)
+		}
+	}
+	return s.pop()
+}
+
+func isOperator(str string) bool {
+	switch str {
+	case "+", "-", "*", "/":
+		return true
+	default:
+		return false
+	}
+}
+
+func doOperator(left, right int, operator string) int {
+	switch operator {
+	case "+":
+		return left + right
+	case "-":
+		return left - right
+	case "*":
+		return left * right
+	case "/":
+		return left / right
+	default:
+		panic("")
+	}
+}
+
+type Stack struct {
+	elems []int
+}
+
+func newStack() *Stack {
+	return &Stack{
+		elems: make([]int, 0),
+	}
+}
+
+func (s *Stack) push(num int) {
+	s.elems = append(s.elems, num)
+}
+
+func (s *Stack) pop() int {
+	elem := s.elems[len(s.elems)-1]
+	s.elems = s.elems[:len(s.elems)-1]
+	return elem
+}
+
+func main() {
+	data := []string{"4", "13", "5", "/", "+"}
+	res := evalRPN(data)
+	fmt.Println(res)
 }
--- top-interview-questions-medium/others/majority-element/solution.go
+++ top-interview-questions-medium/others/majority-element/solution.go
@@ -1,5 +1,27 @@
 package main
 
-func majorityElement(nums []int) int {
+import "fmt"
 
+func majorityElement(nums []int) int {
+	curr := -1
+	count := 0
+	for _, num := range nums {
+		if count == 0 {
+			curr = num
+			count++
+			continue
+		}
+		if num == curr {
+			count++
+		} else {
+			count--
+		}
+	}
+	return curr
+}
+
+func main() {
+	input := []int{2,2,1,1,1,2,2}
+	res := majorityElement(input)
+	fmt.Println(res)
 }
